c2f8ee608b05323148785d288644867397cbe063
Kananate Watewitee <kananate24@gmail.com>
2024-03-02 21:06:23 +0700

    update and delete payments

--- controllers/payments.js
+++ controllers/payments.js
@@ -57,3 +57,69 @@ exports.getPayment = async (req, res, next) => {
         });
     }
 }
+
+//@desc Update one payment
+//@route PUT /api/v1/payments/:id
+//@access registered
+exports.updatePayment = async (req, res, next) => {
+    try {
+        let payment = await Payment.findById(req.params.id);
+
+        if (!payment) {
+            return res.status(404).json({
+                success: false,
+                error: `No payment with the id of ${req.params.id}`
+            });
+        }
+
+        // Add additional checks if needed, such as ensuring the user has permission to update this payment
+
+        payment = await Payment.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            success: true,
+            data: payment
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+
+}
+
+//@desc Delete one payment
+//@route DELETE /api/v1/payments/:id
+//@access registered
+exports.deletePayment = async (req, res, next) => {
+    try {
+        const payment = await Payment.findById(req.params.id);
+
+        if (!payment) {
+            return res.status(404).json({
+                success: false,
+                error: `No payment with the id of ${req.params.id}`
+            });
+        }
+
+        // Add additional checks if needed, such as ensuring the user has permission to delete this payment
+
+        await payment.deleteOne();
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: 'Server Error'
+        });
+    }
+}
