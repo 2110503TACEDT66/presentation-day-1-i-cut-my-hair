f47d4f282918892ecf695adc7e3ebc0cdf223ed8
P-3142857 <144462891+P-3142857@users.noreply.github.com>
2024-03-02 22:56:28 +0900

    Add getme method for auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -63,3 +63,30 @@ exports.login = async (req, res, next) => {
         });
     }
 };
+
+const sendTokenResponse = (user, statusCode, res) => {
+    const token = user.getSignedJwtToken();
+
+    const options = {
+        expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+        httpOnly: true
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
+    res.status(statusCode).cookie('token', token, options).json({
+        success: true,
+        token
+    });
+}
+
+
+exports.getMe = async (req, res, next) => {
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+}
