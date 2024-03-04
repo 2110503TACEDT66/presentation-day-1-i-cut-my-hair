0e6e8b5e26af72cf65a5e736c653a16da0af2219
P-3142857 <144462891+P-3142857@users.noreply.github.com>
2024-03-04 22:07:54 +0900

    Fix bot notification for restaurant

--- bot/notificationRestaurant.js
+++ bot/notificationRestaurant.js
@@ -3,7 +3,7 @@ const client = require('./index');
 const dotenv = require('dotenv');
 dotenv.config({path : 'config/config.env'});
 
-const sendNotification_GetAllRestaurants = (email) => {
+function getTimeNow() {
     let now = new Date(); 
     const options = {
     timeZone: 'Asia/Bangkok', // UTC+7 is Bangkok timezone
@@ -16,21 +16,83 @@ const sendNotification_GetAllRestaurants = (email) => {
     second: '2-digit'
     };
     const dateTime = now.toLocaleString('en-GB', options);
+    return dateTime;
+  }
 
+const sendNotification_GetAllRestaurants = (email,role) => {
     const channel_main_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_MAIN_LOG);  
-    // const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const currentTime = getTimeNow();
 
     if (!channel_main_log) throw new Error('channel_main_log not found');
+    if (!channel_restaurant_log) throw new Error('channel_restaurant_log not found');
 
-    let message = `**Get All Restaurants** ${email} `;
-    message = `[${dateTime}] ${message}`
+    let message = `**Get All Restaurants** ${email} (${role})`;
+    message = `[${currentTime}] ${message}`
     channel_main_log.send(message);
-    // channel_restaurant_log.send(message);
-
+    channel_restaurant_log.send(message);
 };
 
+const sendNotification_GetOneRestaurant = (email,role,nameRestaurant,address) => {
+    const channel_main_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_MAIN_LOG);  
+    const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const currentTime = getTimeNow();
+
+    if (!channel_main_log) throw new Error('channel_main_log not found');
+    if (!channel_restaurant_log) throw new Error('channel_restaurant_log not found');
+
+    let message = `**Get One Restaurant** ${email} (${role}) -> ${nameRestaurant} (${address})`;
+    message = `[${currentTime}] ${message}`
+    channel_main_log.send(message);
+    channel_restaurant_log.send(message);
+};
+
+const sendNotification_CreateRestaurant = (email,role,nameRestaurant,address) => {
+    const channel_main_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_MAIN_LOG);  
+    const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const currentTime = getTimeNow();
+
+    if (!channel_main_log) throw new Error('channel_main_log not found');
+    if (!channel_restaurant_log) throw new Error('channel_restaurant_log not found');
+
+    let message = `**Create Restaurant** ${email} (${role}) -> ${nameRestaurant} (${address})`;
+    message = `[${currentTime}] ${message}`
+    channel_main_log.send(message);
+    channel_restaurant_log.send(message);
+};
+
+const sendNotification_UpdateRestaurant = (email,role,restaurantId) => {
+    const channel_main_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_MAIN_LOG);  
+    const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const currentTime = getTimeNow();
+
+    if (!channel_main_log) throw new Error('channel_main_log not found');
+    if (!channel_restaurant_log) throw new Error('channel_restaurant_log not found');
+
+    let message = `**Update Restaurant** ${email} (${role}) : ${restaurantId}`;
+    message = `[${currentTime}] ${message}`
+    channel_main_log.send(message);
+    channel_restaurant_log.send(message);
+};
+
+const sendNotification_DeleteRestaurant = (email,role,restaurantId) => {
+    const channel_main_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_MAIN_LOG);  
+    const channel_restaurant_log = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID_RESTAURANT_LOG);  
+    const currentTime = getTimeNow();
+
+    if (!channel_main_log) throw new Error('channel_main_log not found');
+    if (!channel_restaurant_log) throw new Error('channel_restaurant_log not found');
+
+    let message = `**Delete Restaurant** ${email} (${role}) : ${restaurantId}`;
+    message = `[${currentTime}] ${message}`
+    channel_main_log.send(message);
+    channel_restaurant_log.send(message);
+};
 
 module.exports = {
     sendNotification_GetAllRestaurants,
-
+    sendNotification_GetOneRestaurant,
+    sendNotification_CreateRestaurant,
+    sendNotification_UpdateRestaurant,
+    sendNotification_DeleteRestaurant
 };
